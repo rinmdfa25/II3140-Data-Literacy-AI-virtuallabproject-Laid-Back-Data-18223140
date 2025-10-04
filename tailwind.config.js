f2e13b1181e2e08f5baa3c031070d387f525123c
Raihan M. Daffa <163854172+rinmdfa25@users.noreply.github.com>
2025-10-04 11:18:39 +0700

    build: fix config on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}", "./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
+export default config;
