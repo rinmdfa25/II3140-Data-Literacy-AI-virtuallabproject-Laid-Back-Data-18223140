9bc44bff4beac1403720af632f47fd1651873555
Raihan M. Daffa <163854172+rinmdfa25@users.noreply.github.com>
2025-10-28 21:54:01 +0700

    docs: add API documentation

---
+++ src/app/api/auth/callback/route.js
@@ -0,0 +1,33 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
+
+  if (code) {
+    const cookieStore = cookies();
+    const supabase = createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY, {
+      cookies: {
+        get(name) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name, value, options) {
+          cookieStore.set({ name, value, ...options });
+        },
+        remove(name, options) {
+          cookieStore.set({ name, value: "", ...options });
+        },
+      },
+    });
+
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (error) {
+      console.error("Authentication Error:", error.message);
+      return NextResponse.redirect(`${origin}/login?error=Could not authenticate user`);
+    }
+  }
+
+  return NextResponse.redirect(`${origin}/home`);
+}
